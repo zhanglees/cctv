e05b6fb0ba110e004dee9eb7172d13b978933bfc
RainbowOak <eezzrrss@sina.com>
2020-12-15 18:40:42 +0800

    put one

---
+++ app/src/main/java/com/bean/CarInfoBean.java
@@ -0,0 +1,29 @@
+package com.bean;
+
+public class CarInfoBean {
+    private String id;
+
+    private String carnum;
+
+    private int numberinfo;
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setCarnum(String carnum) {
+        this.carnum = carnum;
+    }
+    public String getCarnum() {
+        return carnum;
+    }
+
+    public void setNumberinfo(int numberinfo) {
+        this.numberinfo = numberinfo;
+    }
+    public int getNumberinfo() {
+        return numberinfo;
+    }
+}
